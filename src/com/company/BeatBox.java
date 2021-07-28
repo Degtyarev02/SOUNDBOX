574dc84c7bf1330d948c5811420fc5f9ec474043
Vladimir <vladimirdegty@gmail.com>
2021-07-28 12:23:08 +0300

    ref

--- src/com/company/BeatBox.java
+++ src/com/company/BeatBox.java
@@ -37,8 +37,7 @@ public class BeatBox {
             "Cowbell", "VibraSlap", "Low-mid Tom", "High Agogo",
             "Open Hi Conga"};
     int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
-
-
+    
     public static void main(String[] args) {
         new BeatBox().startUp("args[0]");
     }
@@ -68,7 +67,6 @@ public class BeatBox {
 
         checkBoxArrayList = new ArrayList<JCheckBox>();
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
-
 
         JButton start = new JButton("Start");
         start.addActionListener(new MyStartListener());
